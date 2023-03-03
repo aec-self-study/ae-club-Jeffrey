5cc3afd8a90f338fd127e6849a2a2aed53ab4f8d
JeffreyDfE <111977236+JeffreyDfE@users.noreply.github.com>
2023-03-03 14:28:42 +0000

    update staging models

--- coffee_shop_Jeffrey/analyses/codegen.sql
+++ coffee_shop_Jeffrey/analyses/codegen.sql
@@ -7,3 +7,13 @@
     source_name='coffee_shop',
     table_name='orders'
 ) }}
+
+{{ codegen.generate_base_model(
+    source_name='coffee_shop',
+    table_name='product_prices'
+) }}
+
+{{ codegen.generate_base_model(
+    source_name='coffee_shop',
+    table_name='products'
+) }}
